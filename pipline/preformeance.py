b87a58cd836335c4fa25d474cd586c15f7fa4f8e
weixue5 <weixue5@jd.com>
2018-12-20 15:48:39 +0800

    add image_classification pipline

---
+++ pipline/preformeance.py
@@ -0,0 +1,159 @@
+import sklearn
+from sklearn import metrics
+from sklearn.preprocessing import label_binarize
+import json
+import argparse
+import numpy
+
+parser = argparse.ArgumentParser(description='manual to this script')
+parser.add_argument('--result_dir', type=str, default = None)
+parser.add_argument('--output_dir', type=str, default = None)
+#parser.add_argument('--look_up_dictionary_dir', type=str, default = None)
+
+args = parser.parse_args()
+result_dir = args.result_dir
+output_dir = args.output_dir
+#look_up_dictionary_dir = args.look_up_dictionary_dir
+'''
+labels_dictionary_path = look_up_dictionary_dir + 'labels_dictionary.json'
+with open(labels_dictionary_path) as load_f:
+  labels_dictionary = json.load(load_f)
+
+labels_dictionary_list =[]
+for i in labels_dictionary:
+  labels_dictionary_list.append(labels_dictionary[i])
+print(labels_dictionary_list)
+'''
+result_path = result_dir + 'result.json'
+with open(result_path) as load_f:
+  result = json.load(load_f)
+
+files = result['files']
+
+labels = list(str(result['labels'][1:-1]).split(', '))
+l = []
+for i in labels:
+  try:
+    l.append(int(i))
+  except:
+    continue
+labels = l
+
+logits = result['logits']
+preds = [i.index(max(i)) for i in logits]
+
+num_class = len(logits[0])
+print(num_class)
+labels_one_hot = label_binarize(labels, classes=range(num_class))
+
+classif_per = {}
+accuracy_score = metrics.accuracy_score(labels,preds)
+classif_per['accuracy'] = accuracy_score
+print(accuracy_score)
+
+precision_score = metrics.precision_score(labels,preds,average='macro')
+classif_per['precision'] = precision_score
+print(precision_score)
+
+f1_score = metrics.f1_score(labels,preds,average='macro')
+classif_per['f1_score'] = f1_score
+print(f1_score)
+
+recall_score = metrics.recall_score(labels,preds,average='macro')
+classif_per['recall_score'] = recall_score
+print(recall_score)
+import numpy
+'''
+roc_auc_score_list = []
+
+for i in range(num_class):
+  ll = numpy.asarray(labels) == i
+  log = []
+  for j in logits:
+    log.append(j[i])
+#  print(log)
+  roc_auc_score_list.append(metrics.roc_auc_score(ll,log, average='macro'))
+roc_auc_score_macro=numpy.mean(roc_auc_score_list)
+'''
+roc_auc_score_micro = metrics.roc_auc_score(labels_one_hot, logits, average='micro')
+'''
+try :
+  roc_auc_score_macro = metrics.roc_auc_score(labels_one_hot, logits, average='macro')
+except:
+  roc_auc_score_macro = numpy.nan
+
+classif_per['auc_macro'] = roc_auc_score_macro
+'''
+classif_per['auc_micro'] = roc_auc_score_micro
+print(roc_auc_score_micro)
+classification_report_dict = {}
+classification_report = metrics.classification_report(labels,preds,labels=range(num_class))
+classification_report = str(classification_report).split('\n')
+
+for i in range(len(classification_report)):
+  x = classification_report[i]
+  x = str(x).split(' ')
+  xx =[]
+  for j in x:
+    try:
+      assert len(j)>0
+      xx.append(j)
+    except:
+      continue
+#  print(len(xx))
+  if len(xx) == 4:
+    classification_report_dict['evaluation_index'] = xx
+  elif len(xx) == 7:
+    classification_report_dict['avg_all'] = xx[3:]
+  elif len(xx)>0:
+    classification_report_dict[xx[0]]=xx[1:]
+print(classification_report_dict)
+
+classif_per['classification_report'] = classification_report_dict
+#print('classification_report' + str(type(classification_report)))
+confusion_matrix = metrics.confusion_matrix(labels,preds)
+confusion_matrix_str = ''
+#print(type(confusion_matrix)) <type 'numpy.ndarray'>
+for i in confusion_matrix:
+  #print(type(i)) <type 'numpy.ndarray'>
+#  confusion_matrix_.append(list(i))
+#  print(numpy.shape(i))
+  for j in i:
+    confusion_matrix_str = confusion_matrix_str + str(j) + '\t'
+  confusion_matrix_str = confusion_matrix_str + '\n'
+print(confusion_matrix_str)
+'''
+confusion_matrix_dic = {}
+confusion_matrix_dic['confusion_matrix'] = confusion_matrix_
+print(confusion_matrix_dic)
+'''
+classif_per_path = output_dir + 'result.json'
+import json
+jsObj = json.dumps(classif_per)
+fileObject = open(classif_per_path, 'w')
+fileObject.write(jsObj)
+fileObject.close()
+
+confusion_matrix_path = output_dir + 'confusion_matrix.txt'
+confusion_matrix_file = open(confusion_matrix_path,'w')
+confusion_matrix_file.write(confusion_matrix_str)
+confusion_matrix_file.close()
+
+correct_path =  output_dir + 'correct.txt'
+error_path = output_dir + 'error.txt'
+right = open(correct_path,'w')
+error = open(error_path,'w')
+for i in range(len(files)):
+  f = files[i]
+  l = labels[i]
+  p = preds[i]
+  if l == p:
+    right.write(f + '\t' + str(l) + '\t' + str(p) + '\n')
+  else:
+    error.write(f + '\t' + str(l) + '\t' + str(p) + '\n')
+right.close()
+error.close()
+
+
+
+
--- pipline/train_early_stop.py
+++ pipline/train_early_stop.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 # the same as neuhub
 import os
-
+import numpy
 from tensorflow.contrib.data.python.ops import batching
 from tensorflow.contrib.data.python.ops import interleave_ops
 from tensorflow.contrib.data.python.ops import prefetching_ops
@@ -12,16 +12,18 @@ import Configure_file
 tf.app.flags.DEFINE_string('pre_trained_model_ckpt_path', '', '')
 tf.app.flags.DEFINE_string('checkpoint_dir', '/weixue/my_bench/imagenet/checkpoint/', '')
 tf.app.flags.DEFINE_string('summary_dir', '/weixue/my_bench/imagenet/checkpoint/', '')
-tf.app.flags.DEFINE_string('data_dir', '/weixue/imagenet-tf/', '')
+tf.app.flags.DEFINE_string('train_data_dir', '/weixue/imagenet-tf/', '')
+tf.app.flags.DEFINE_string('validation_data_dir', '/weixue/imagenet-tf/', '')
+tf.app.flags.DEFINE_string('export_path_base', '', 'export file')
 
-tf.app.flags.DEFINE_integer('batch_size', 64, '')
-tf.app.flags.DEFINE_integer('input_height', 224, '')
-tf.app.flags.DEFINE_integer('input_width', 224, '')
+tf.app.flags.DEFINE_integer('batch_size', 16, '')
+#tf.app.flags.DEFINE_integer('input_height', 224, '')
+#tf.app.flags.DEFINE_integer('input_width', 224, '')
 
 #tf.app.flags.DEFINE_integer('num_examples', 7000, 'the number of training samples')
 
 tf.app.flags.DEFINE_integer('num_class', 37, 'the number of training sample categories')
-tf.app.flags.DEFINE_integer('epochs', 100, '')
+tf.app.flags.DEFINE_float('epochs', 10 , '')
 tf.app.flags.DEFINE_integer('warmup_epochs', 0, '')
 
 tf.app.flags.DEFINE_float('init_learning_rate', 0.0001, '')
@@ -31,12 +33,12 @@ tf.app.flags.DEFINE_float('warm_lr', 0.1, '')
 
 tf.app.flags.DEFINE_string('lr_decay', 'exponential_decay', 'exponential_decay, natural_exp_decay,polynomial_decay')
 tf.app.flags.DEFINE_string('optimizer', 'rmsp', 'rmsp,adam,sgd,mometum,lars')
-tf.app.flags.DEFINE_string('model_name', 'resnet_v2_101', 'rmsp,adam,sgd,mometum,lars')
+tf.app.flags.DEFINE_string('model_name', 'vgg_16','')
 
 tf.app.flags.DEFINE_integer('display_every_steps', 10, '')
-tf.app.flags.DEFINE_boolean('fine_tune', True, 'whether the model is trained from a pre-trained model')
-
-tf.app.flags.DEFINE_string('export_path_base', '', 'export file')
+tf.app.flags.DEFINE_integer('eval_every_steps', 100, '')
+tf.app.flags.DEFINE_integer('fine_tune', 1, 'whether the model is trained from a pre-trained model')
+tf.app.flags.DEFINE_integer('early_stop', 1, 'whether to stop training model early')
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -44,25 +46,28 @@ tf.logging.set_verbosity(tf.logging.INFO)
 slim = tf.contrib.slim
 
 batch_size = FLAGS.batch_size
-height = FLAGS.input_height
-width = FLAGS.input_width
+import Configure_file
+height = Configure_file.configure_image_size(FLAGS.model_name)
+width = height
 display_every_steps = FLAGS.display_every_steps
 
 def preprocess_fn(value, batch_position, is_training):
+
     from pre import parse_example_proto
     image_buffer, label, label_text, filename = parse_example_proto(value)
+    
+    from pre import preprocess
+    images = preprocess(image_buffer,is_training, FLAGS.model_name, height, width)
 
-    from pre import preprocess_resnet
-    #  is_training = tf.Print(is_training,[is_training])
-    images = preprocess_resnet(image_buffer=image_buffer, is_training=is_training)
-    print('input image height %d' %height)
     return (images, label, label_text, filename)
 
 def train_data_generator():
     with tf.name_scope('train_batch_processing'):
-        data_dir = FLAGS.data_dir
+        data_dir = FLAGS.train_data_dir
         glob_pattern = os.path.join(data_dir, 'train-*-of-*')
         file_names = gfile.Glob(glob_pattern)
+        import random
+        random.shuffle(file_names)
         ds = tf.data.TFRecordDataset.list_files(file_names)
 
         ds = ds.apply(interleave_ops.parallel_interleave(tf.data.TFRecordDataset, cycle_length=10))
@@ -85,7 +90,7 @@ def train_data_generator():
 
 def validation_data_generator():
     with tf.name_scope('validation_batch_processing'):
-        data_dir = FLAGS.data_dir
+        data_dir = FLAGS.validation_data_dir
         glob_pattern = os.path.join(data_dir, 'validation-*-of-*')
         file_names = gfile.Glob(glob_pattern)
         ds = tf.data.TFRecordDataset.list_files(file_names)
@@ -109,27 +114,38 @@ def validation_data_generator():
 def main(argv=None):
 	
     import json
-    num_examples_path = FLAGS.data_dir + 'num_examples.json'
-    with open(num_examples_path) as load_f:
+    train_num_examples_path = FLAGS.train_data_dir + 'num_examples.json'
+    validation_num_examples_path = FLAGS.validation_data_dir + 'num_examples.json'
+    with open(train_num_examples_path) as load_f:
       load_dict = json.load(load_f)     
-      num_examples = load_dict['the total number of available samples']
+      train_num_examples = load_dict['the total number of available samples']
+    with open(validation_num_examples_path) as load_f:
+      load_dict = json.load(load_f)     
+      validation_num_examples = load_dict['the total number of available samples']
       
-    max_steps = int(FLAGS.epochs * num_examples) / (FLAGS.batch_size)
-    max_steps = 1
     num_classes = FLAGS.num_class
-
-    warm_steps = int(FLAGS.warmup_epochs * num_examples / (FLAGS.batch_size))
-
+    eval_every_steps = FLAGS.eval_every_steps
+    train_steps = int(FLAGS.epochs * train_num_examples) / (FLAGS.batch_size)
+    eval_steps = int(validation_num_examples / (FLAGS.batch_size))
+    warm_steps = int(FLAGS.warmup_epochs * train_num_examples / (FLAGS.batch_size))
+    fine_tune_flag = 'True' if FLAGS.fine_tune == 1 else 'False'
+    early_stop_flag = 'True' if FLAGS.early_stop == 1 else 'False'
     print('---' * 20)
-    print('number of training samples : %d' % num_examples)
-    print('number of training samples : %d' % FLAGS.num_class)
-    print('max steps : %d' % max_steps)
+    print('model for classification : %s' % FLAGS.model_name)
+    print('input height and width : %d' % height)
+    print('whether to fine tune : %s' % fine_tune_flag)
+    print('whether to early stop : %s' % early_stop_flag)
+    print('number of train samples : %d' % train_num_examples)
+    print('number of train classes : %d' % FLAGS.num_class)
+    print('eval every steps training %d' % FLAGS.eval_every_steps)
+    print('train steps : %d' % train_steps)
+    print('eval steps : %d' % eval_steps)
     print('batch size : %d' % batch_size)
     print('optimizer : %s' % FLAGS.optimizer)
     print('init_learning rate :%f' % FLAGS.init_learning_rate)
     print('lr deay policy :%s' % FLAGS.lr_decay)
-    print('warm learning rate :%f' % FLAGS.warm_lr)
     print('warm epochs : %d' % FLAGS.warmup_epochs)
+    print('warm learning rate :%f' % FLAGS.warm_lr)
     print('---' * 20)
 
     global_steps = tf.train.get_or_create_global_step()
@@ -137,23 +153,19 @@ def main(argv=None):
     lr = Configure_file.configure_lr(FLAGS.init_learning_rate, FLAGS.lr_decay, FLAGS.decay_steps,
                       FLAGS.decay_rate, global_steps, FLAGS.warm_lr, warm_steps)
     # (init_lr,decay_policy,decay_steps,decay_rate,global_steps,warm_lr=0.0001,warm_steps=0)
-
-    tf.summary.scalar('learning_rate', lr)
-
+#    tf.summary.scalar('learning_rate', lr)
     opt = Configure_file.configure_optimizer(FLAGS.optimizer, lr)
     # (optimizer,learning_rate)
 
     train_dataset = train_data_generator()
     validation_dataset = validation_data_generator()
-
-    iterator = tf.data.Iterator.from_structure(output_types=train_dataset.output_types,
-                                               output_shapes = train_dataset.output_shapes)
-
+    iterator = tf.data.Iterator.from_structure(output_types=train_dataset.output_types,output_shapes = train_dataset.output_shapes)
     train_init_op = iterator.make_initializer(train_dataset)
     validation_init_op = iterator.make_initializer(validation_dataset)
 
     images, labels, label_texts, filenames = iterator.get_next()
     
+#    images = tf.Print(images,[filenames])
     images = tf.reshape(images, shape=[-1, height, width, 3])
     labels = tf.reshape(labels, [batch_size])
 
@@ -163,28 +175,20 @@ def main(argv=None):
         num_classes = num_classes,
         weight_decay = 0.0004,
         is_training = True)
-
     logits, end_points = network_fn(images)
     pred_soft = tf.nn.softmax(logits)
-#    print(pred_soft)
-    values, indices = tf.nn.top_k(pred_soft, 1)
-    
+    values, indices = tf.nn.top_k(pred_soft, 1)    
     if 'AuxLogits' in end_points:
         aux_cross_entropy = 0.4 * tf.reduce_mean(
-    tf.nn.sparse_softmax_cross_entropy_with_logits(logits=end_points['AuxLogits'], labels=labels,
-                                                   name='aux_cross-entropy'))
+    tf.nn.sparse_softmax_cross_entropy_with_logits(logits=end_points['AuxLogits'], labels=labels,name='aux_cross-entropy'))
     else:
         aux_cross_entropy = 0
-
-    cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=logits, labels=labels, name='cross-entropy'))
-
+    cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels, name='cross-entropy'))
     loss = cross_entropy + aux_cross_entropy
-
     with tf.name_scope('accuracy'):
         top_1 = tf.reduce_mean(tf.cast(tf.nn.in_top_k(pred_soft, labels, 1), dtype=tf.float32), name='top_1')
         top_5 = tf.reduce_mean(tf.cast(tf.nn.in_top_k(pred_soft, labels, 5), dtype=tf.float32), name='top_5')
-
+    '''
     tf.summary.scalar('cross_entropy', cross_entropy)
     tf.summary.scalar('aux_cross_entropy', aux_cross_entropy)
     tf.summary.scalar('loss', loss)
@@ -193,7 +197,7 @@ def main(argv=None):
 
     for i in tf.global_variables():
         tf.summary.histogram(i.name.replace(":", "_"), i)
-
+    '''
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     with tf.control_dependencies(update_ops):
         train_step = opt.minimize(loss, global_step=global_steps)
@@ -203,7 +207,7 @@ def main(argv=None):
         train_op = tf.no_op(name='train_op')
 
     with tf.control_dependencies([loss, top_1, top_5]):  # , variables_averages_op,batch_norm_updates_op]):
-        eval_op = tf.no_op(name='eval_op')
+        validation_op = tf.no_op(name='validation_op')
 
     config = tf.ConfigProto()
     config.allow_soft_placement = True
@@ -211,21 +215,21 @@ def main(argv=None):
     checkpoint_dir = FLAGS.checkpoint_dir
     summary_dir = FLAGS.summary_dir
     summary_op = tf.summary.merge_all()
-#    saver = tf.train.Saver(tf.global_variables(), max_to_keep=1)
     saver = tf.train.Saver(tf.trainable_variables(),max_to_keep=1)
     
     checkpoint_basename = FLAGS.model_name + '.ckpt'
     hooks = [
-        tf.train.LoggingTensorHook(
-            tensors={'step': global_steps, 'loss': loss, 'lr': lr, 'top1': top_1, 'top5': top_5},
-            every_n_iter=FLAGS.display_every_steps),
-        tf.train.StopAtStepHook(num_steps=None, last_step=max_steps),
+#        tf.train.LoggingTensorHook(
+#            tensors={'step': global_steps, 'loss': loss, 'lr': lr, 'top1': top_1, 'top5': top_5},
+#            every_n_iter=FLAGS.display_every_steps),
+#        tf.train.StopAtStepHook(num_steps=None, last_step=max_steps),
         tf.train.CheckpointSaverHook(checkpoint_dir=checkpoint_dir, save_steps=100, saver=saver,
                                  checkpoint_basename=checkpoint_basename),
-        tf.train.SummarySaverHook(save_steps=100, save_secs=None, output_dir=summary_dir, summary_writer=None,
-                              scaffold=None, summary_op=summary_op)]
+#        tf.train.SummarySaverHook(save_steps=100, save_secs=None, output_dir=summary_dir, summary_writer=None,
+#                              scaffold=None, summary_op=summary_op)
+    ]
 
-    if FLAGS.fine_tune:
+    if FLAGS.fine_tune == 1:
         exclusions = Configure_file.model_exclusions(FLAGS.model_name)
         print(exclusions)
         variables_to_restore = []
@@ -236,8 +240,6 @@ def main(argv=None):
             else:
               variables_to_restore.append(var)
 
-        print('variables to restore is :')
-        print(variables_to_restore)
         pre_train_saver = tf.train.Saver(variables_to_restore)
 
         if tf.gfile.IsDirectory(FLAGS.pre_trained_model_ckpt_path):
@@ -266,29 +268,68 @@ def main(argv=None):
         scaffold = tf.train.Scaffold(init_fn=load_pretrain, summary_op=tf.summary.merge_all())
     else:
         scaffold = None
+      
+    print('start training')
+    early_stop_param = {}
+    early_stop_param['count'] = 0
+    early_stop_param['last'] = 100000
+    
     with tf.train.MonitoredTrainingSession(checkpoint_dir=None,
                                            config=config,
                                            scaffold=scaffold,
                                            hooks=hooks
                                            ) as mon_sess:
+      if FLAGS.early_stop == 1:
+        print('early stop')
         mon_sess._coordinated_creator.tf_sess.run(train_init_op)
-        print('start training')
-
-        while not mon_sess.should_stop():
+        
+        import early_stop
+        global_step = 0
+        while global_step < train_steps:
+          if (global_step) % eval_every_steps == 0 and global_step > 0:
+            print('start validating')
+            mon_sess._coordinated_creator.tf_sess.run(validation_init_op)
+            loss_list = []
+            tt1 = []
+            for i in range(eval_steps) :
+              _, batch_loss, t1 = mon_sess.run([validation_op, loss, top_1])
+              loss_list.append(batch_loss)
+              tt1.append(t1)
+            validation_loss = numpy.mean(numpy.asarray(loss_list))
+            validation_top1 = numpy.mean(numpy.asarray(tt1))
+            mon_sess._coordinated_creator.tf_sess.run(train_init_op)
+            print('done validating, validation loss is %f , top1 is %f' % (validation_loss,validation_top1))
+            global_step = global_step + 1
+            early_stop_param = early_stop.early_stop(validation_loss,early_stop_param)
+            if early_stop_param['count'] >= 3:
+              print('process should stop')
+              break
+          if (global_step+1) % display_every_steps == 0 and global_step > 0:
+            global_step, _, batch_loss, top1, top5 = mon_sess.run([global_steps, train_op, loss, top_1, top_5])
+            print('global_step: %d, train_loss: %f, top1: %f, top5: %f' % (global_step, batch_loss, top1, top5))
+          else: 
             global_step, _ = mon_sess.run([global_steps, train_op])
-        print('done training...')
-    
-            # Export model
-            # WARNING(break-tutorial-inline-code): The following code snippet is
-            # in-lined in tutorials, please update tutorial documents accordingly
-            # whenever code changes.
+      else:
+        print('no early stop')
+        mon_sess._coordinated_creator.tf_sess.run(train_init_op)
+        global_step = 0
+        while global_step < train_steps:
+          if (global_step+1) % display_every_steps == 0 and global_step > 0:
+            global_step, _, batch_loss, top1, top5 = mon_sess.run([global_steps, train_op, loss, top_1, top_5])
+            print('global_step: %d, train_loss: %f, top1: %f, top5: %f' % (global_step, batch_loss, top1, top5))
+          else: 
+            global_step, _ = mon_sess.run([global_steps, train_op])
+          
+	# Export model
+	# WARNING(break-tutorial-inline-code): The following code snippet is
+	# in-lined in tutorials, please update tutorial documents accordingly
+	# whenever code changes.
 #    mon_sess.graph._unsafe_unfinalize()
     export_path_base = FLAGS.export_path_base
     export_path = os.path.join(
       tf.compat.as_bytes(export_path_base),
-      tf.compat.as_bytes('image_class/')
-      )
-    
+      tf.compat.as_bytes(str('image_class'))
+    )
     print('Exporting trained model to', export_path)
     builder = tf.saved_model.builder.SavedModelBuilder(export_path)
     # Build the signature_def_map.
@@ -318,9 +359,11 @@ def main(argv=None):
     
     latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
     saver = tf.train.Saver(tf.trainable_variables())
+
     with tf.Session() as sess:
-      sess.run([tf.global_variables_initializer()])
+      sess.run(tf.global_variables_initializer())
       saver.restore(sess, latest_checkpoint)
+      legacy_init_op = tf.group(tf.tables_initializer(), name='legacy_init_op')
       builder.add_meta_graph_and_variables(
         sess, [tf.saved_model.tag_constants.SERVING],
         signature_def_map={
